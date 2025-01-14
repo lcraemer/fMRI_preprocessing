e50f6046f69f2367eb086ac6eac6e8590c0f8cac
lcraemer <lcraemer@gmx.de>
2025-01-14 13:10:27 +0100

    Update runPreprocessing.m

--- runPreprocessing.m
+++ runPreprocessing.m
@@ -25,20 +25,19 @@ clear all
 parent_dir = fileparts(pwd);
 
 % SPM12
-spm_path = fullfile(parent_dir,'toolboxes/spm/spm12/');
+spm_path = 'C:\Users\craemerl\Documents\MATLAB\GitHub\spm12';
 addpath(spm_path)
 spm('defaults','fmri')
 spm_jobman('initcfg')
 
 % TAPAS toolbox 
-tapas_path = fullfile(parent_dir,'toolboxes/tapas-master');
+tapas_path = 'C:\Users\craemerl\Documents\MATLAB\GitHub\tapas';
 addpath(tapas_path)
 
 % Data source root directory
 % E.g., ds_root = '~/Documents/gb_fmri_data/BIDS/ds_xxx';
-ds_root = fullfile(parent_dir,'data/ds004331-download');
+ds_root =  'G:\Pilot_P8_MRT\BIDS';
 src_dir = 'func';  % functional data sub-directory
-
 
 % Subject directories
 % E.g., sub_dir = {'sub-01', 'sub-02', 'sub-03'};
@@ -46,12 +45,12 @@ sub_dir = dir(fullfile(ds_root,'sub*'));
 sub_dir = {sub_dir.name}';
 
 % Data target directory 
-tgt_dir = '/Users/johannessinger/Documents/cloud_Berlin/Projekte/fmri_pipeline/derived'; 
+tgt_dir = 'G:\Pilot_P8_MRT\derived';
 
 % BIDS format file name part labels
-BIDS_fn_label{1} = {'_task-localizer';'_task-main'}; % BIDS file name task label
+BIDS_fn_label{1} = '_task-gb'; % BIDS file name task label
 BIDS_fn_label{2} = ''; % BIDS file name acquisition label
-BIDS_fn_label{3} = '_run-00'; % BIDS file name run index
+BIDS_fn_label{3} = '_run-0'; % BIDS file name run index
 BIDS_fn_label{4} = '_bold'; % BIDS file name modality suffix
 
 % Preprocessing object
@@ -61,7 +60,7 @@ BIDS_fn_label{4} = '_bold'; % BIDS file name modality suffix
 % E.g., run_sel = {[1 2 3 4 5 6], [1 2 3 4 5 6]};
 % first vector in the first cell is for subject 1 and for the first task, second vector is for the second task
 for i = 1:length(sub_dir)
-    run_sel{i} = {[1],[1:12]};
+    run_sel{i} = {[10 12 14 16]};
 end 
 
 % Select preprocessing steps 
@@ -75,7 +74,7 @@ end
 %       6. Estimation of noise regressors using the aCompCor method
 %       (Behzadi,2018) 
 %       7. Smoothing (optional) 
-prep_steps = [0:7];
+prep_steps = [0:1];
 
 % Preprocessing variables
 prep_vars = struct();
@@ -85,19 +84,13 @@ prep_vars.src_dir = src_dir;
 prep_vars.tgt_dir = tgt_dir;
 prep_vars.BIDS_fn_label = BIDS_fn_label;
 prep_vars.prep_steps = prep_steps;
-prep_vars.nslices = 57;
-prep_vars.TR = 1.5;
-prep_vars.slicetiming = [0, 0.78, 0.0775, 0.8575, 0.1575, 0.935, 0.235, 1.0125,...
-                        0.3125, 1.09, 0.39, 1.17, 0.4675, 1.2475, 0.545, 1.325,...
-                        0.625, 1.4025, 0.7025, 0, 0.78, 0.0775, 0.8575, 0.1575,...
-                        0.935, 0.235, 1.0125, 0.3125, 1.09, 0.39, 1.17, 0.4675,...
-                        1.2475, 0.545, 1.325, 0.625, 1.4025, 0.7025, 0, 0.78,...
-                        0.0775, 0.8575, 0.1575, 0.935, 0.235, 1.0125, 0.3125,...
-                        1.09, 0.39, 1.17, 0.4675, 1.2475, 0.545, 1.325, 0.625, 1.4025, 0.7025];
+prep_vars.nslices = 72;
+prep_vars.TR = 1;
+prep_vars.slicetiming = [0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002,0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002,0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002,0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002,0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002,0.5,0.09000000000003183,-0.32000000000005,0.25,-0.1499999999999773,0.4199999999999591,0.009999999999990905,-0.3999999999999773,0.1699999999999591,-0.2400000000000091,0.3400000000000318,-0.07000000000005002];                     
 
 % Cycle over participants
 % -----------------------
-for i = 2%:numel(sub_dir)
+for i = 1:numel(sub_dir)
     
     prep_vars.run_sel = run_sel{i};
 
