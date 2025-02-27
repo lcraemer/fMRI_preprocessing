aaaf100ebdb3986307eeb47fae8c5bb1f29eaedd
lcraemer <lcraemer@gmx.de>
2025-02-27 17:38:04 +0100

    Update get_cond_order.m

--- get_cond_order.m
+++ get_cond_order.m
@@ -1,18 +1,18 @@
 % function to obtain the order of conditions that were presented in
-% different runs for the objdraw experiment
+% different runs
 function cond_order=get_cond_order(prep,i_sub)
 
-ons_names = dir(fullfile(prep.tgt_dir,i_sub,'onsets','*task-main*'));
+ons_names = dir(fullfile(prep.tgt_dir,i_sub,'onsets','*Predator*'));
 ons_names = {ons_names.name}';
-conds = {'Photo', 'Drawing', 'Sketch'};
+conds = {'Prediction';'WaitScene';'Outcome';'ITI'};
 
 for i=1:length(ons_names)
-    
+
     load(fullfile(prep.tgt_dir,i_sub,'onsets',ons_names{i}))
-    
-    cond_idx = find(strcmpi(conds,names{1}(1:end-2)));
+
+    cond_idx = find(strcmpi(conds, names{1}));
     
     cond_order(i) = cond_idx;
-end 
-    
-end 
+end
+
+end
